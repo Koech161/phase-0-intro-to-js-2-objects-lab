48cce2da1773952956118de7aa262313731425c5
Abel Koech <koechabel161@gmail.com>
2024-07-01 10:44:58 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee={
+    name:'Abel',
+    streetAddress:'12 Kabarak',
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee={...employee};
+    newEmployee[key]=value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+        employee[key]=value;
+        return employee
+} destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'sam');
+ function deleteFromEmployeeByKey(employee, key){
+        const {name, ...newEmployee}= employee;
+        return newEmployee;
+ }
+ function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+ }
